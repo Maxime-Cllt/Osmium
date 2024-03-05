13f4093c5f395945e4bfeaa395add460d6efb10e
Maxime <98154358+Maxime-Cllt@users.noreply.github.com>
2024-03-05 18:45:07 +0100

    Optimisation et cleanup du code

--- osmium/bin/compression.ml
+++ osmium/bin/compression.ml
@@ -1,14 +1,14 @@
 open Gsl;;
 
 let convert_array_float_to_int array_float =
-    Array.map (fun ligne -> Array.map int_of_float ligne) array_float;;
+  Array.map (Array.map int_of_float) array_float;;
 
 let convert_array_int_to_float array_int =
-    Array.map (fun ligne -> Array.map float_of_int ligne) array_int;;
+  Array.map (Array.map float_of_int) array_int;;
 
 (* Effectue le compression du tableau de taille MxN selon le compression_rate (un pourcentage du rang de la matrice)
 Renvoie une matrie MxN de range K, qui est le résultat de la SVD compressée *)
-let make_compression array compression_rate verbose =
+let make_compression array compression_rate =
     (* Ajoute autant de ligne que nécessaire pour avoir une matrice carrée *)
     let pad_matrix matrix nb_row nb_column=
         let array_matrix = Matrix.to_arrays matrix in
@@ -21,11 +21,10 @@ let make_compression array compression_rate verbose =
         let nb_row, nb_column = ((Array.length array), (Array.length array.(0))) in
         let padding = nb_row < nb_column in
         (* Padding dans le cas où l'image est en mode paysage (nb_ligne<nb_colonne)*)
-        let matrix = if padding then (pad_matrix (Matrix.of_arrays array) nb_row nb_column) else (Matrix.of_arrays array) in 
-        let nb_row = (fst (Matrix.dims matrix)) in
-        (nb_row, nb_column, matrix, padding) in
+        let matrix = if padding then pad_matrix (Matrix.of_arrays array) nb_row nb_column else Matrix.of_arrays array in
+        let nb_row = fst (Matrix.dims matrix) in
+        nb_row, nb_column, matrix, padding in
     (* Effectue la Svd avec le librairie Gsl renvoie (Array(MxN), Array(N), Array(NxN)) *)
-
 
     let exec_svd matrix =
         let (_, nb_column) = Matrix.dims matrix in
@@ -43,7 +42,7 @@ let make_compression array compression_rate verbose =
 
 
     (* Redéfinir taux de compression comme étant le pourcentage de valeur singulière à garder plutôt que le pourcentage de colonnes à garder*)
-    let compress_svd arrays_u array_s arrays_v nb_row nb_column compression_rate verbose=
+    let compress_svd arrays_u array_s arrays_v nb_row nb_column compression_rate =
         let nb_comp_column = int_of_float ((float_of_int (List.length (List.filter (fun x -> x <> 0. ) (Array.to_list array_s)))) *. compression_rate) in
 
         let vecMat_u_comp = Vectmat.mat_convert (`M (Matrix.of_arrays (Array.map (fun row -> Array.sub row 0 nb_comp_column) arrays_u))) in (* Compression de u en matrice de taille MxK*)
@@ -61,7 +60,6 @@ let make_compression array compression_rate verbose =
         let vecMat_inter = Vectmat.mat_convert (`M (Matrix.create ?init:(Some 0.) nb_row nb_comp_column)) in (* MxK *)
         let vecMat_res = Vectmat.mat_convert (`M (Matrix.create ?init:(Some 0.) nb_row nb_column)) in (* MxN *)
 
-        if verbose then (   (* Optionnel pour avoir des informations supplémentaires sur la matrice en console *)
             let sum_all_SV = Array.fold_left (+.) 0. array_s in
             let sum_comp_SV = Array.fold_left (+.) 0. array_s_comp in
             let ratio = sum_comp_SV /. sum_all_SV in
@@ -70,10 +68,8 @@ let make_compression array compression_rate verbose =
             Printf.printf "Taille de l'image = (%d, %d)\n" nb_row nb_column;
             Printf.printf "Rang de l'image = %d; Rang de la matrice compressée = %d\n" (Array.length non_zero_s) (Array.length (Vectmat.to_arrays vecMat_s_comp));
             print_newline ();
-        );
 
         (vecMat_u_comp, vecMat_s_comp, vecMat_vT_comp, vecMat_inter, vecMat_res) in
-
 
     assert(compression_rate <= 1.);
         (*  M        N     MxN/NxN  bool *)
@@ -81,11 +77,10 @@ let make_compression array compression_rate verbose =
         (*  MxN       N       NxN *)
     let (arrays_u, array_s, arrays_v) = exec_svd matrix in
         (*     MxK          KxK            KxN             MxK           MxN     avec K le rang de la matrice renvoyée, calculé avec le taux de compression*)
-    let (vecMat_u_comp, vecMat_s_comp, vecMat_vT_comp, vecMat_inter, vecMat_res) = compress_svd arrays_u array_s arrays_v nb_row nb_column compression_rate verbose in
+    let (vecMat_u_comp, vecMat_s_comp, vecMat_vT_comp, vecMat_inter, vecMat_res) = compress_svd arrays_u array_s arrays_v nb_row nb_column compression_rate in
 
     Linalg.matmult ~a:vecMat_u_comp ~b:vecMat_s_comp vecMat_inter;
     Linalg.matmult ~a:vecMat_inter ~b:vecMat_vT_comp vecMat_res;
 
-    (*if padded then (Vectmat.to_arrays vecMat_res) (* Pour vérifier que le padding n'a pas d'effet sur la SVD *)*)
     if padded then Array.sub (Vectmat.to_arrays vecMat_res) 0 (Array.length array) (* Retire les lignes qui ont été ajoutées*)
     else Vectmat.to_arrays vecMat_res;;
--- osmium/bin/main.ml
+++ osmium/bin/main.ml
@@ -1,63 +1,36 @@
-(* let image = "../documentation/images/femme_carre.jpeg" in (* carrée *) *)
- let image = "../documentation/images/gmk.jpeg" in
-(*let image = "../documentation/images/femme_portrait.jpeg" in (* portrait *)*)
-(*let image = "../documentation/images/homme_carre.jpeg" in (* carrée *)*)
-(*let image = "../documentation/images/homme_paysage.jpeg" in (* paysage *)*)
-(*let image = "../documentation/images/homme_portrait.jpeg" in (* portrait *)*)
+ let image = "../documentation/images/gmk100.jpeg" in
+
 let taux_compression = 0.50 in
 let array_image = Graphic_image.array_of_image (Jpeg.load image []) in
 
 let (array_image_red, array_image_green, array_image_blue) = Affichage.get_colors array_image in
 print_newline ();
-Printf.printf "Compression matrice rouge...\n";
-let image_red_compresse = Compression.convert_array_float_to_int
-    (Compression.make_compression
-        (Compression.convert_array_int_to_float array_image_red) taux_compression true) in
-Printf.printf "Compression matrice verte...\n";
-let image_green_compresse = Compression.convert_array_float_to_int
-    (Compression.make_compression
-        (Compression.convert_array_int_to_float array_image_green) taux_compression true) in
-Printf.printf "Compression matrice bleu...\n";
-let image_blue_compresse = Compression.convert_array_float_to_int
-    (Compression.make_compression
-        (Compression.convert_array_int_to_float array_image_blue) taux_compression true) in
 
+(* Compression *)
+let compress_and_convert color_array =
+  let float_array = Compression.convert_array_int_to_float color_array in
+  let compressed_array = Compression.make_compression float_array taux_compression in
+  Compression.convert_array_float_to_int compressed_array in
 
+Printf.printf "Compression de la matrice rouge...\n";
+let image_red_compresse = compress_and_convert array_image_red in
+Printf.printf "Compression de la matrice verte...\n";
+let image_green_compresse = compress_and_convert array_image_green in
+Printf.printf "Compression de la matrice bleu...\n";
+let image_blue_compresse = compress_and_convert array_image_blue in
 
 let image_compresse = Affichage.assign_value image_red_compresse image_green_compresse image_blue_compresse in
-(* let process_2d_array arr = *)
-(*  Array.iter (fun inner_arr -> Array.iter print_int inner_arr; print_newline ()) arr in *)
 Printf.printf "Compression terminée.\n";
 Printf.printf "Taille de l'image originale : %d\n" (Array.length array_image);
 Printf.printf "Taille de l'image compressée : %d\n" (Array.length image_compresse);
-Printf.printf "Taux de compression : %f\n" (float_of_int (Array.length image_compresse) /. float_of_int (Array.length array_image));
-print_newline ();
 let compare_result image image_compresse =
-    (* Fonction qui permet de créer une session interactive que l'on peut quitter avec la touche 'q' *)
     Graphics.open_graph "";
-
     (* Voir *)
     let graphe_image_compresse = Graphics.make_image image_compresse in
     let offset_x, offset_y = 50, 100 in
-    (*let graphe_image_compresse = Graphics.make_image (Array.map (Array.map (fun pixel -> Graphics.rgb (pixel lsr 16 land 0xff) (pixel lsr 8 land 0xff) (pixel land 0xff))) image_compresse) in (* Création du tablean en l'image graphique avec le RGB, source d'erreur potentielle*)*)
     let array_image = Graphic_image.array_of_image (Jpeg.load image []) in (* Ouverture de l'image de base*)
-
     Graphics.draw_image (Graphics.make_image array_image) offset_x offset_y; (* Affichage de l'image de base *)
     Graphics.draw_image graphe_image_compresse ((Array.length array_image.(0))+offset_y) offset_y; (* Affichage de l'image compressée*)
     Jpeg.save "../documentation/compresse/image_compresse.jpeg" [] (Images.Rgb24 (Graphic_image.image_of graphe_image_compresse)) in
 
 compare_result image image_compresse;;
-
-(* process_2d_array image_compresse *)
-(* let save_2d_array arr filename =
-let oc = open_out filename in
-  Array.iter (fun inner_arr ->
-    Array.iter (fun x ->
-      output_string oc (string_of_int x ^ " ")
-    ) inner_arr;
-    output_string oc "\n"
-  ) arr;
-  close_out oc in
-  save_2d_array image_compresse "test.text"; *)
-
-
