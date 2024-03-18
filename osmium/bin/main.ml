b27055251b169edbbeb8de164ee27a3058ececd1
Maxime <98154358+Maxime-Cllt@users.noreply.github.com>
2024-03-18 18:39:43 +0100

    Test approfondi + Préparation utilisation theads

--- osmium/bin/main.ml
+++ osmium/bin/main.ml
@@ -1,5 +1,11 @@
-let image = "../documentation/images/gmk100.jpeg" in
-let taux_compression = 1.0 in
+(* let image = "../documentation/images/gmk166x100.jpeg" in *)
+(* let image = "../documentation/images/ws200x200.jpeg" in *)
+(* let image = "../documentation/images/ngannou1000x500.jpg" in *)
+(* let image = "../documentation/images/CL1600x1000.jpeg" in *)
+ let image = "../documentation/images/lh2560x1707.jpg" in
+
+let file_name = Filename.chop_extension (Filename.basename image) in
+let taux_compression = 0.1 in
 let array_image = Graphic_image.array_of_image (Jpeg.load image []) in
 let (array_image_red, array_image_green, array_image_blue) = Affichage.get_colors array_image in
 
@@ -16,11 +22,16 @@ let image_green_compresse = compress_and_convert array_image_green in
 Printf.printf "Compression de la matrice bleu...\n";
 let image_blue_compresse = compress_and_convert array_image_blue in
 
+(* Affichage de l'image originale *)
 let image_compresse = Affichage.assign_value image_red_compresse image_green_compresse image_blue_compresse in
 Printf.printf "Compression terminée.\n";
 Printf.printf "Taille de l'image originale : %d\n" (Array.length array_image);
 Printf.printf "Taille de l'image compressée : %d\n" (Array.length image_compresse);
+
+(* Sauvegarde de l'image compressé *)
 Graphics.open_graph "";
 let graphe_image_compresse = Graphics.make_image image_compresse in
-Jpeg.save "../documentation/compresse/image_compresse.jpeg" [] (Images.Rgb24 (Graphic_image.image_of graphe_image_compresse));
+(* Jpeg.save "../documentation/compresse/image_compresse.jpeg" [] (Images.Rgb24 (Graphic_image.image_of graphe_image_compresse)); *)
+let file_dest = Printf.sprintf "../documentation/compresse/image_compresse_%s_%.2f.jpeg" file_name taux_compression in
+Jpeg.save file_dest [] (Images.Rgb24 (Graphic_image.image_of graphe_image_compresse));
 Graphics.close_graph ();
